be4ff0582326f8c5b4bfc85338ea69b1d5be7bfc
Summer Kiflain <summersyed@yahoo.com>
2025-11-30 17:15:59 -0500

    navigate from header to watch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,25 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { useRoute } from '@react-navigation/native'
+import { useRoute, useNavigation } from '@react-navigation/native'
 
 import SearchIcon from '../assets/svgs/searchIcon.svg'
 
 export function Header() {
   const insets = useSafeAreaInsets()
   const route = useRoute()
+  const navigation = useNavigation()
   return (
     <View style={[styles.mainContainer, { paddingTop: insets.top }]}>
       <View style={styles.headerBar}>
         <Text style={styles.headerTitle}>{route.name}</Text>
 
-        <TouchableOpacity onPress={() => {}}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('HomeTabs', {
+              screen: 'Watch',
+            })
+          }
+        >
           <SearchIcon width={34} height={34} fill={'#202C43'} />
         </TouchableOpacity>
       </View>
